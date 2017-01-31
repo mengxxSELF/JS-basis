1e7f1a0c4f2d6a975145aacf0d6003be3441007c
webmaxxSELF <webmxxself@163.com>
2017-01-31 17:26:31 +0800

    publick

---
+++ js/6task.js
@@ -0,0 +1,12 @@
+/*判断是否为公有方法的封装*/
+
+function publicWay(attr,obj){
+    return (attr in obj)&&(obj.hasOwnProperty(attr) );
+}
+
+function Person(){
+    this.name='ok'
+}
+Person.prototype.age=20;
+console.log(publicWay('age',publicWay))
+console.log(publicWay('name',publicWay))
