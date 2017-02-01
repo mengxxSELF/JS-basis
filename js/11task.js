5766771dfc7cbaca0c5bb7227c01301e2cba2eba
webmaxxSELF <webmxxself@163.com>
2017-02-01 09:28:46 +0800

    99

--- js/11task.js
+++ js/11task.js
@@ -1,2 +1,20 @@
 /* 99乘法表*/
 
+~function () {
+    var oUl = document.getElementsByTagName('ul')[0];
+    var alis = document.getElementsByTagName('li');
+    var str ='';
+    for(var i=1;i<10;i++){
+        str +='<li>';
+        for(var j=1;j<i;j++){
+            str +='<span>'+j+'x'+i+'='+(j*i)+'</span>';
+        }
+        str +='</li>';
+    }
+    oUl.innerHTML = str;
+    // 各行变色
+    for(var i=0 ,len=alis.length;i<len;i++){
+        alis[i].className= i%2==0?'event':'odd'
+    };
+
+}();
